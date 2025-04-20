b14847b8486b6f78a2358674f04a0593b0d8eecb
Pallapothu Manikanta <pallapothum784@gmail.com>
2025-04-20 17:27:28 +0530

    Initialize react project using vite and cleaned the source code to understand react flow

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Mani from "./mani"
+
+function App() {
+  return (
+    <>
+    <Mani/>
+    </>
+  )
+}
+
+export default App
---
+++ vitereact/src/mani.jsx
@@ -0,0 +1,11 @@
+function Mani(){
+    return (
+        <>
+        <h1>Name:Pallapothu Manikanta!</h1>
+        <h2>Section:6AIMLB</h2>
+        <h3>Concept:Learning React from Chai Aur Code Youtube Channel</h3>
+        </>
+    )
+}
+
+export default Mani;
