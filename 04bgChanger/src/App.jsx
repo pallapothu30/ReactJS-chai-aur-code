c824d80dd98351a4de8b4239d124e7f1d4bf53ed
Pallapothu Manikanta <pallapothum784@gmail.com>
2025-05-04 01:24:41 +0530

    some change

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className='fixed flex flex-wrap justify-center top-15 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-7 shadow-lg bg-white px-3 py-2 rounded-4xl'>
           <button onClick = {()=>setColor("red")}className='outline-none cursor-pointer px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"red"}}>red</button>
-          <button onClick = {()=>setColor("purple")}className='outline-none cursor-pointer px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"purple"}}>blue</button>
+          <button onClick = {()=>setColor("purple")}className='outline-none cursor-pointer px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"purple"}}>purple</button>
           <button onClick = {()=>setColor("green")}className='outline-none cursor-pointer px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"green"}}>green</button>
           <button onClick = {()=>setColor("black")}className='outline-none cursor-pointer px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"black"}}>black</button>
           <button onClick = {()=>setColor("blue")}className='outline-none cursor-pointer px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"blue"}}>blue</button>
