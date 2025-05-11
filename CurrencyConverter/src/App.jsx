0ba35553ad250809609dc6d1e1485e48c775af50
Pallapothu Manikanta <pallapothum784@gmail.com>
2025-05-11 22:32:38 +0530

    Currency Converter:Custom hook with reusing components

---
+++ CurrencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <h1 className="text-center mt-10 text-3xl">
+      <span className="inline-block bg-black text-white px-4 py-2 rounded-full">
+        Currency Converter App
+      </span>
+    </h1>
+  )
+}
+
+export default App
