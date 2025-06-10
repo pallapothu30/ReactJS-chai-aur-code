9b9025335458be190367ce698f00d18c208ebff4
Pallapothu Manikanta <pallapothum784@gmail.com>
2025-06-10 18:18:16 +0530

    improved code

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -3,10 +3,7 @@ import UserContext from "../context/UserContext";
 
 function Profile(){
     const {user} = useContext(UserContext)
-    if(!user){
-        return <div>Please login</div>
-    }
-    return <div>Welcome {user.username}</div>
+    return <div>Welcome {user?.username}</div>
 }
 
 export default Profile;
